5fdfbdd9282410b8528dd121b6fc2adf106f2611
Karin Karlsson <karin.drake@gmail.com>
2020-08-17 23:42:10 +0200

    övning 5 är klar

--- readData.js
+++ readData.js
@@ -1,2 +1,34 @@
+const fs=require('fs');
+const fsReader=fs.createReadStream('./personData.json')
+
 //övning 5
+
+// fs.readFile('./personData.json', (error, data)=>{
+//     if(error){
+//         console.log("Något gick fel: ", error)
+//         return;
+//     }
+//     personObject = JSON.parse(data);
+//     console.log("Övning 5:")
+//     console.log("Namn: ", personObject.firstname)
+//     console.log("Hårfärg: ", personObject.haircolor)
+
+
+// })
+
+//Övning 5b
+
+let data="";
+
+fsReader.on('data', chunk => {
+
+    console.log("Data hämtad: ", chunk);
+    data+=chunk;
+    
+});
+fsReader.on('end', () => {
+    let object=JSON.parse(data);
+    console.log("Förnamnet är: ",object.firstname)
+	console.log('Nu är strömmen slut, det kommer ingen mer data.');
+});
 
